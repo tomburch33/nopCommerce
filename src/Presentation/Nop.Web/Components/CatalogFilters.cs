36af635760cd0394ff1bfe2ecd90aba5a4dae52a
holydk <omigoneo@gmail.com>
2020-12-21 14:00:48 +0300

    issue 276 Move filters into component

---
+++ src/Presentation/Nop.Web/Components/CatalogFilters.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+using Nop.Web.Models.Catalog;
+
+namespace Nop.Web.Components
+{
+    public class CatalogFiltersViewComponent : NopViewComponent
+    {
+        public CatalogFiltersViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(int currentCategoryId, int currentManufacturerId)
+        {
+            if (currentCategoryId > 0)
+                return PrepareCategoryFilters(currentCategoryId);
+            else
+            if (currentManufacturerId > 0)
+                return PrepareManufacturerFilters(currentManufacturerId);
+            else
+                return Content(string.Empty);
+        }
+
+        private IViewComponentResult PrepareCategoryFilters(int categoryId)
+        {
+            return View(new CatalogProductsFilteringModel());
+        }
+
+        private IViewComponentResult PrepareManufacturerFilters(int manufacturerId)
+        {
+            return View(new CatalogProductsFilteringModel());
+        }
+    }
+}
